1b67b3d7db352a0308af37ad5221a7ce705ee728
Anik-the-dev <niksctg00@gmail.com>
2022-07-06 11:09:11 +0600

    random name generate done

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function Home() {
+  const names = ["Anik", "Joffer", "Moyank", "Charlee", "Premo", "Levos"];
+  const getRandom = () => {
+    const a = names[Math.floor(Math.random() * names.length)];
+    return a;
+  };
+  const name = getRandom()
+  console.log(getRandom());
   return (
     <View>
-      <Text>Home Anik Bhai</Text>
+      <Text>Home Anik Bhai {name}</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
