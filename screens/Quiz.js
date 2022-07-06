b6782dbddc82eb3880c30c3f3b96c320f316d561
Anik-the-dev <niksctg00@gmail.com>
2022-07-06 13:16:24 +0600

    questions shows in UI from API:

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,58 +1,65 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Quiz({ navigation }) {
   const [ques, setQues] = useState();
-  const getQuestions = async () =>{
-      const url = 'https://opentdb.com/api.php?amount=11';
-      const res = await fetch(url);
-      const data = await res.json()
-      console.log(data.results)
-      setQues(data.results)
-  }
- 
-  useEffect(()=>{
-     getQuestions();
-  },[])
+  const [quesNo, setQuesNo] = useState(0);
+  const getQuestions = async () => {
+    const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results);
+    setQues(data.results);
+  };
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  console.log(ques);
   return (
     <View style={styles.quizContainer}>
-      <View style={styles.top}>
-        <Text style={styles.screenTextH}>Q.This is a simple question</Text>
-      </View>
+      {ques && (
+        <View style={styles.parent}>
+          <View style={styles.top}>
+            <Text style={styles.screenTextH}>Q.{ques[quesNo].question}</Text>
+          </View>
 
-      <View style={styles.options}>
-        <View>
-          <TouchableOpacity>
-            <Text style={styles.screenTextP}>Option ONE</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity>
-            <Text style={styles.screenTextP}>Option TWO</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity>
-            <Text style={styles.screenTextP}>Option THREE</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity>
-            <Text style={styles.screenTextP}>Option FOUR</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.bottom}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Skip</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-        {/* <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Result")}>
+          <View style={styles.options}>
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.screenTextP}>Option ONE</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.screenTextP}>Option TWO</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.screenTextP}>Option THREE</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.screenTextP}>Option FOUR</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.bottom}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Skip</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+            {/* <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Result")}>
           <Text>End</Text>
         </TouchableOpacity> */}
-      </View>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -62,6 +69,9 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: "20%",
     marginHorizontal: 22,
+  },
+  parent:{
+      flex:1
   },
   top: {},
   options: {
@@ -86,12 +96,11 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: "#FFD764",
     borderRadius: 5,
-    paddingHorizontal:16,
-    paddingVertical:8
-
+    paddingHorizontal: 16,
+    paddingVertical: 8,
   },
-  buttonText:{
-      fontWeight:"500",
-      fontSize:16
-  }
+  buttonText: {
+    fontWeight: "500",
+    fontSize: 16,
+  },
 });
