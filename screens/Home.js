fc098de933187943422d70118aae2d51a28e5252
Anik-the-dev <niksctg00@gmail.com>
2022-07-06 11:33:01 +0600

    home page image done

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,Image, TouchableOpacity } from "react-native";
 import React from "react";
+import home from '../images/home.png'
 
 export default function Home() {
-  const names = ["Anik", "Joffer", "Moyank", "Charlee", "Premo", "Levos"];
+  // random name generator............
+  const names = ["Anik", "Joffer", "Moyank", "Charlee", "Premo", "Levos", "Thrive"];
   const getRandom = () => {
     const a = names[Math.floor(Math.random() * names.length)];
     return a;
   };
   const name = getRandom()
-  console.log(getRandom());
   return (
-    <View>
-      <Text>Home Anik Bhai {name}</Text>
+    <View style={styles.container}>
+      <Image style={styles.imageDimentions} source={home}></Image>
+      <TouchableOpacity
+        style={{backgroundColor:"#3d3d3d", borderRadius:50}}
+        onPress={()=>{}}
+      >
+          <Text style={{color:"#fff", fontWeight:'600', fontSize:18, padding:16}}>Start Quiz</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height:"100%",    
+    alignItems: "center",
+    backgroundColor:"#0c6291"
+  },
+  imageDimentions:{
+    marginVertical:50,
+    width:"80%",
+    height:"40%"
+  }
+});
